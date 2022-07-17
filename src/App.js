a39c57450223a2d739ff8abc9e3a5bbefa7ef51f
Gurramgadda Sai Nithin <sairahul9999999@gmail.com>
2022-07-17 21:50:27 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import TextForm from "./components/TextForm";
 import React, {useState} from 'react';
 import Alerts from "./components/Alerts";
 import About from "./components/About";
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
+import {BrowserRouter as Router, Routes, Route,Link} from "react-router-dom"
 
 function App() {
   const [mode, setMode] = useState('light');
@@ -45,9 +45,9 @@ function App() {
       <Alerts alert={alert}/>
       <div className="container my-3 ">
         <Routes>
-          <Route path='/' element={<TextForm heading="Welcome to TextUtils you can Convert your Text here" mode = {mode} showAlert={showAlert} />}>
+          <Route exact path='/' element={<TextForm heading="Welcome to TextUtils you can Convert your Text here" mode = {mode} showAlert={showAlert} />}>
           </Route>
-          <Route path='/about' element={<About mode = {mode}/>}>
+          <Route exact path='/about' element={<About mode = {mode}/>}>
           </Route>
         </Routes>
       </div>
